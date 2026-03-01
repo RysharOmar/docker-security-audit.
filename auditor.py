574d4461d66df7477c6e19e33dc9d6ba33493389
RysharOmar <rysharperez@gmail.com>
2026-03-01 15:38:53 -0500

    Update auditor.py

--- auditor.py
+++ auditor.py
@@ -29,7 +29,7 @@ def check_dockerfile(dockerfile_path):
 # Recorrer archivos (buscando Dockerfile o Dockerfile.txt)
 for root, dirs, files in os.walk(dockerfile_directory):
     for file in files:
-        if file.startswith('Dockerfile'):
+        if file.startswith('DockerfileSeguro'):
             check_dockerfile(os.path.join(root, file))
 
 # Imprimir resultados
